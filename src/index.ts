3797966b9fb4f7cb81cc8ae43aa60ad8a88f9a0c
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2020-09-29 09:09:35 +0200

    Added 3 diff .env's so we can test at the same time

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
+const current =
+  process.env.NODE_ENV != "production" ? "." + process.env.NODE_ENV : "";
 dotenv.config({
-  path: process.env.NODE_ENV === "development" ? `.env.dev` : ".env",
+  path: current,
 }); //Dotenv lets you get env values (for example we can do process.env.bot_token)
 import("./languages/Language");
 import("./bot/bot");
