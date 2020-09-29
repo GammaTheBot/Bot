30bfeace387add8fbf7824aee436f927f7560b57
MiginsMC <thefiregod2511@gmail.com>
2020-09-29 22:24:01 +1000

    Update help.ts

--- src/bot/commands/Utility/help.ts
+++ src/bot/commands/Utility/help.ts
@@ -1,51 +1,70 @@
 import { Language, Lang } from "../../../languages/Language";
 import { ArgType, Command, commands, categories } from "../../commandLoader";
-import Discord from 'discord.js';
-import { bot } from "../../bot"
+import Discord from "discord.js";
+// import { bot } from "../../bot";
 
-export var Eval: Command = {
+export var Help: Command = {
   category: "Utility",
   name: "help",
-  description: Language.getNode(Lang.English, "command.help.description"),
+  usage: "help [command]",
+  description: (guild) =>
+    Language.getNode(guild, ["command", "help", "description"]),
   clientPermissions: [],
   args: [
     {
       unordered: false,
       type: ArgType.lowercase,
       match: "everything",
-      description: "The command to describe",
       optional: true,
+      name: "command",
     },
   ],
   dms: true,
-  exec: async (message, [text]: [string]) => {
-    if (!text) {
-        const helpEmbed = new Discord.MessageEmbed()
+  examples: ["help", "help eval"],
+  exec: async (message, { command }: { command: string }) => {
+    if (!command) {
+      const helpEmbed = new Discord.MessageEmbed()
         .setColor("BLUE")
         .setTitle("Gamma Help")
         .setTimestamp()
-        .setDescription("Execute `!help <command>` to find information on specific commands!")
+        .setDescription(
+          "Execute `!help <command>` to find information on specific commands!"
+        )
         .setAuthor(message.author.tag, message.author.displayAvatarURL());
-        for (const key of Object.values(categories)) {
-            if (key.commands) {
-                helpEmbed.addField(`${key.name}`, `${key.commands.map((e) => `\`${e.name}\``).join(", ")}`)
-            }
+      for (const key of Object.values(categories)) {
+        if (key.commands) {
+          helpEmbed.addField(
+            `${key.name}`,
+            `${key.commands.map((e) => `\`${e.name}\``).join(", ")}`
+          );
         }
-        return message.channel.send(helpEmbed);
+      }
+      return message.channel.send(helpEmbed);
     }
-    const command = commands.find(
-        (c) => c.name === text || c.aliases?.includes(text)
+    const cmd = commands.find(
+      (c) => c.name === command || c.aliases?.includes(command)
     );
     const helpEmbed = new Discord.MessageEmbed()
-    .setColor("BLUE")
-    .setTitle(`Gamma Help - ${command.name}`)
-    .setTimestamp()
-    .setAuthor(message.author.tag, message.author.displayAvatarURL())
-    .setDescription(
-        `Name: \`${command.name}\`\nDescription: \`delta did this bad, will add later lol\`\nCategory: \`${command.category}\`\nAliases: \`${command.aliases ? command.aliases.join(", ") : "None"}\``
-        
-    );
+      .setColor("BLUE")
+      .setTitle(`Gamma Help - ${cmd.name}`)
+      .setTimestamp()
+      .setAuthor(message.author.tag, message.author.displayAvatarURL())
+      .setDescription(
+        `Name: \`${cmd.name}\`` +
+          `\n${cmd.usage ? `Usage: \`${cmd.usage}\`` : ``}` +
+          `\n${
+            cmd.aliases
+              ? `${
+                  cmd.aliases.length > 1 ? `Aliases: ` : `Alias:`
+                } \`${cmd.aliases.join(", ")}\``
+              : ``
+          }` +
+          `\nCategory: \`${cmd.category}\`` +
+          `\nDescription: \`${cmd.description(message.guild.id)}\`` +
+          `\n${
+            cmd.examples.length > 1 ? `Examples: ` : `Example: `
+          } ${cmd.examples.map((m) => `\`${m}\``).join(", ")}`
+      );
     return message.channel.send(helpEmbed);
-
   },
 };
