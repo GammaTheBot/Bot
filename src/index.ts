12c3c544c413edfb1bca483aa6de9e866e70bf16
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2020-09-29 09:13:56 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 const current =
   process.env.NODE_ENV != "production" ? "." + process.env.NODE_ENV : "";
 dotenv.config({
-  path: current,
+  path: ".env" + current,
 }); //Dotenv lets you get env values (for example we can do process.env.bot_token)
 import("./languages/Language");
 import("./bot/bot");
